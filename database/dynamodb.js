0c92fdabcb2b6742b8e31a4e81e51af2e57aa2f5
jdub <potluckyum@gmail.com>
2020-06-07 10:57:06 -0400

    Adds database explorer for local

--- database/dynamodb.js
+++ database/dynamodb.js
@@ -2,16 +2,30 @@
 
 const Promise = require("bluebird");
 const AWS = require("aws-sdk");
+const {createServer} = require('dynamodb-admin');
 
-var dynamodbOfflineOptions = {
+const dynamodb = new AWS.DynamoDB();
+const dynClient = new AWS.DynamoDB.DocumentClient({service: dynamodb});
+
+const dynamodbOfflineOptions = {
     region: "localhost",
     endpoint: "http://localhost:8000"
-  },
-  isOffline = () => process.env.IS_OFFLINE;
+  };
 
-var client = isOffline()
+var client = process.env.IS_OFFLINE
   ? new AWS.DynamoDB.DocumentClient(dynamodbOfflineOptions)
   : new AWS.DynamoDB.DocumentClient();
+
+if(process.env.IS_OFFLINE){
+  const app = createServer(dynamodb, dynClient);
+
+  const port = 8001;
+  const server = app.listen(port);
+  server.on('listening', () => {
+    const address = server.address();
+    console.log(`  listening on http://0.0.0.0:${address.port}`);
+  });
+}
 
 module.exports = (method, params) => {
   return Promise.fromCallback(cb => client[method](params, cb));
